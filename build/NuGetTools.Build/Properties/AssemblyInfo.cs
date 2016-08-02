20dae38b01c79a3f9f84355cf93d6d412936c0e7
Joel Verhagen <jver@microsoft.com>
2016-08-02 08:33:06 -0700

    Add blank AssemblyInfo.cs to all projects

---
+++ build/NuGetTools.Build/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyMetadata("CommitHash", "0000000000000000000000000000000000000000")]
