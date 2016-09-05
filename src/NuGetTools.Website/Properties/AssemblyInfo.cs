eac7d0d5326251cedefdf05e5fe1b45f1e47ec39
Joel Verhagen <joel.verhagen@gmail.com>
2016-09-05 11:58:11 -0700

    Remove assembly info (it should be ignored)

--- src/NuGetTools.Website/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
-[assembly: AssemblyMetadata("CommitHash", "0000000000000000000000000000000000000000")]
