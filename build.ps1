e524302e67a2bb979c82d3f4d6d21b494041e21e
Joel Verhagen <joel.verhagen@gmail.com>
2017-01-07 14:40:25 -0800

    Add skip parameters to the build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,12 @@
+param (
+    [switch] $SkipPrepare,
+    [switch] $SkipRestore,
+    [switch] $SkipBuild,
+    [switch] $SkipPackageDownload,
+    [switch] $SkipTests,
+    [switch] $SkipPublish
+)
+
 $root = $PSScriptRoot
 $lastTraceTime = Get-Date
 
@@ -51,47 +60,72 @@ function Show-ErrorExitCode
     }
 }
 
-$dotnetCliDir = Join-Path $root "cli"
-$dotnet = Join-Path $dotnetCliDir "dotnet.exe"
+if (-Not $SkipPrepare) {
+    Trace-Information "Preparing build environment..."
+    $dotnetCliDir = Join-Path $root "cli"
+    $dotnet = Join-Path $dotnetCliDir "dotnet.exe"
 
-New-Item $dotnetCliDir -Force -Type Directory | Out-Null
-$dotnetCliUrl = "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.ps1"
-$dotnetCliInstallScript = Join-Path $dotnetCliDir "dotnet-install.ps1"
-Invoke-WebRequest $dotnetCliUrl -OutFile $dotnetCliInstallScript
+    New-Item $dotnetCliDir -Force -Type Directory | Out-Null
+    $dotnetCliUrl = "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.ps1"
+    $dotnetCliInstallScript = Join-Path $dotnetCliDir "dotnet-install.ps1"
+    Invoke-WebRequest $dotnetCliUrl -OutFile $dotnetCliInstallScript
 
-& $dotnetCliInstallScript -InstallDir $dotnetCliDir -Version 1.0.0-preview2-003156
-Show-ErrorExitCode
-
-Trace-Information "Restoring projects..."
-& $dotnet restore $root
-Show-ErrorExitCode
-
-Trace-Information "Generating assembly info..."
-& $dotnet run -p (Get-BuildProject -Name "Knapcode.NuGetTools.Build") -- assemblyInfo --baseDirectory $root
-Show-ErrorExitCode
-
-Trace-Information "Building..."
-$projectsToBuild = Get-ChildItem $root -Recurse -Include "project.json"
-foreach ($projectToBuild in $projectsToBuild)
-{
-   & $dotnet build $projectToBuild
-   Show-ErrorExitCode
+    & $dotnetCliInstallScript -InstallDir $dotnetCliDir -Version 1.0.0-preview2-003156
+    Show-ErrorExitCode
+} else {
+    Trace-Information "Skipped prepare."
 }
 
-Trace-Information "Downloading NuGet packages for the website..."
-& $dotnet run -p (Get-SrcProject -Name "Knapcode.NuGetTools.PackageDownloader") -- (Join-Path (Get-SrcDir -Name "Knapcode.NuGetTools.Website") "packages")
-Show-ErrorExitCode
-
-Trace-Information "Testing..."
-$projectsToTest = Get-ChildItem (Join-Path $root "test") -Recurse -Include "project.json"
-foreach ($projectToTest in $projectsToTest)
-{
-   & $dotnet test $projectToTest
-   Show-ErrorExitCode
+if (-Not $SkipRestore) {
+    Trace-Information "Restoring projects..."
+    & $dotnet restore $root
+    Show-ErrorExitCode
+} else {
+    Trace-Information "Skipped restore."
 }
 
-Trace-Information "Publishing the website..."
-& $dotnet publish (Get-SrcProject -Name "Knapcode.NuGetTools.Website")
-Show-ErrorExitCode
+if (-Not $SkipBuild) {
+    Trace-Information "Generating assembly info..."
+    & $dotnet run -p (Get-BuildProject -Name "Knapcode.NuGetTools.Build") -- assemblyInfo --baseDirectory $root
+    Show-ErrorExitCode
+
+    Trace-Information "Building..."
+    $projectsToBuild = Get-ChildItem $root -Recurse -Include "project.json"
+    foreach ($projectToBuild in $projectsToBuild)
+    {
+       & $dotnet build $projectToBuild
+       Show-ErrorExitCode
+    }
+} else {
+    Trace-Information "Skipped build."
+}
+
+if (-Not $SkipPackageDownload) {
+    Trace-Information "Downloading NuGet packages for the website..."
+    & $dotnet run -p (Get-SrcProject -Name "Knapcode.NuGetTools.PackageDownloader") -- (Join-Path (Get-SrcDir -Name "Knapcode.NuGetTools.Website") "packages")
+    Show-ErrorExitCode
+} else {
+    Trace-Information "Skipped package download."
+}
+
+if (-Not $SkipTests) {
+    Trace-Information "Testing..."
+    $projectsToTest = Get-ChildItem (Join-Path $root "test") -Recurse -Include "project.json"
+    foreach ($projectToTest in $projectsToTest)
+    {
+       & $dotnet test $projectToTest
+       Show-ErrorExitCode
+    }
+} else {
+    Trace-Information "Skipped tests."
+}
+
+if (-Not $SkipPublish) {
+    Trace-Information "Publishing the website..."
+    & $dotnet publish (Get-SrcProject -Name "Knapcode.NuGetTools.Website")
+    Show-ErrorExitCode
+} else {
+    Trace-Information "Skipped publish."
+}
 
 Trace-Information "Build complete."
