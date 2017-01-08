900e8ae4d7dc06e127668f49fa595d1378178e58
Joel Verhagen <joel.verhagen@gmail.com>
2017-01-07 19:13:19 -0800

    Add bootstrap3-typeahead

--- src/Knapcode.NuGetTools.Website/wwwroot/_references.js
+++ src/Knapcode.NuGetTools.Website/wwwroot/_references.js
@@ -2,6 +2,7 @@
 /// <reference path="../gulpfile.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/bootstrap3-typeahead/bootstrap3-typeahead.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
