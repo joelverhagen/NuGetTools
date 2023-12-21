90c943fcf28ae1635f5083ea17cd82407d4d0cc8
Joel Verhagen <jver@microsoft.com>
2023-12-20 19:22:28 -0500

    Remove comment

--- src/Knapcode.NuGetTools.Website/Program.cs
+++ src/Knapcode.NuGetTools.Website/Program.cs
@@ -1,6 +1,5 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddNuGetTools();
